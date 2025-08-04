45ac99ff55cd470aefb03dbe536a8455c5191c85
Luis Segoviano <the.segoviano@gmail.com>
2025-08-04 16:14:47 -0600

    Commit inicial

---
+++ technical-test-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  technical-test-ios
+//
+//  Created by Luis Segoviano on 04/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ technical-test-ios/technical_test_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  technical_test_iosApp.swift
+//  technical-test-ios
+//
+//  Created by Luis Segoviano on 04/08/25.
+//
+
+import SwiftUI
+
+@main
+struct technical_test_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
